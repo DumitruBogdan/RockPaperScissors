4832444ff7d2e515aac86589de2f2ee56f1610bf
DumitruBogdan <90133050+DumitruBogdan@users.noreply.github.com>
2022-05-05 12:40:04 +0300

    Initial Commit

---
+++ RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Bogdan on 05.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
